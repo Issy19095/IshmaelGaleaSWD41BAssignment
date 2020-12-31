d93e34645b6b1f16a65955149aac378452c821af
Issy19095 <73161801+Issy19095@users.noreply.github.com>
2020-12-31 10:35:36 +0100

    Created Player & Move
    I changed a couple of things with the background and found some new assets. I inserted the player and created a script to move.

---
+++ Home/2DCarGame/Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 0.7f;
+    [SerializeField] float health = 200;
+
+    float xMin, xMax, yMin, yMax;
+
+    void Start()
+    {
+        SetUpMoveBoundaries();
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        //get the main camera from Unity
+        Camera gameCamera = Camera.main;
+        //set boundaries on the x-axis
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+
+        //set boundaries on the y-axis
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var newXPos = transform.position.x + deltaX;
+        newXPos = Mathf.Clamp(newXPos, xMin, xMax);
+
+
+        this.transform.position = new Vector2(newXPos, -2.3f);
+    }
+}
---
+++ Home/2DCarGame/Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    [SerializeField] float backgroundScrollSpeed = 0.5f;
+
+    //the material from the texture
+    Material myMaterial;
+
+    //the movement
+    Vector2 offSet;
+
+    void Start()
+    {
+        myMaterial = GetComponent<Renderer>().material;
+        //scroll in the y axis at that speed
+        offSet = new Vector2(0f, backgroundScrollSpeed);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myMaterial.mainTextureOffset += offSet * Time.deltaTime;
+    }
+}
